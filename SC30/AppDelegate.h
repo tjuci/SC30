31e00f8c5b40ce9948cc9a1616c6d45fbd04c586
andrew <tjuci@sina.com>
2017-06-07 13:36:56 +0800

    Initial Commit

---
+++ SC30/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SC30
+//
+//  Created by Andrew-mac on 2017/6/7.
+//  Copyright © 2017年 SITRI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SC30/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SC30
+//
+//  Created by Andrew-mac on 2017/6/7.
+//  Copyright © 2017年 SITRI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
